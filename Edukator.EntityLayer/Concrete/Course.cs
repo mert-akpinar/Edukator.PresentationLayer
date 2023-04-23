12d92b2c1d824077fe20edb3d3c6c3dcda020179
mert-akpinar <106198602+mert-akpinar@users.noreply.github.com>
2023-04-23 23:45:57 +0300

    Add project files.

---
+++ Edukator.EntityLayer/Concrete/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrete
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Review { get; set; }
+    }
+}
