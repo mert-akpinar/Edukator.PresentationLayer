048627e002eacd09255817250d1e92baf0f875f2
mert-akpinar <106198602+mert-akpinar@users.noreply.github.com>
2023-05-11 19:42:33 +0300

    SweetAlert

---
+++ Edukator.PresentationLayer/Controllers/SweatAlertController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class SweatAlertController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Edukator.PresentationLayer/Models/RegisterViewModel.cs
+++ Edukator.PresentationLayer/Models/RegisterViewModel.cs
@@ -1,14 +1,17 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Edukator.PresentationLayer.Models
 {
-	public class RegisterViewModel
-	{
-		public string Name { get; set; }
-		public string Surname { get; set; }
-		public string City { get; set; }
-		public string UserName { get; set; }
-		public string Email { get; set; }
-		public string Password { get; set; }
-	}
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 }
